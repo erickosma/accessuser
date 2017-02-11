cd030ce89bf2f651ede5bd77efb1745a61874834
Erick Giorgio <erickosma@gmail.com>
2017-02-11 19:04:32 -0200

    migration

--- src/migrations/2017_02_11_151851_add_refer_to_tables.php
+++ src/migrations/2017_02_11_151851_add_refer_to_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddReferToTables extends Migration
+class AddReferToTables extends MigrationAccess
 {
 
     protected $table = 'access_agents';
